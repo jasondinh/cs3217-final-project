9c612bc70959ef5783c1d16fd058952636d33d22
Jason Dinh <bathanh@gmail.com>
2011-03-25 20:18:29 +0800

    initial project

---
+++ MallExplorer/Classes/MallExplorerAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  MallExplorerAppDelegate.h
+//  MallExplorer
+//
+//  Created by bathanh-m on 3/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MallExplorerViewController;
+
+@interface MallExplorerAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    MallExplorerViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet MallExplorerViewController *viewController;
+
+@end
+
