742f15a7b9d54bc553a9a516a0c89e846f12595a
Dam Tuan Long <damlong-m@soctf-imac-026.comp.nus.edu.sg>
2011-04-02 04:38:56 +0800

    fix bugs

---
+++ MallExplorer/MasterViewController.h
@@ -0,0 +1,20 @@
+//
+//  MallListViewController.h
+//  MallExplorer
+//
+//  Created by Dam Tuan Long on 3/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CityMapViewController;
+@interface MasterViewController : UINavigationController {
+	
+	CityMapViewController* cityMapViewController;
+}
+
+@property (nonatomic,retain) IBOutlet CityMapViewController* cityMapViewController;
+
+@end
+
---
+++ MallExplorer/MasterViewController.m
@@ -0,0 +1,214 @@
+//
+//  MallListViewController.m
+//  MallExplorer
+//
+//  Created by Dam Tuan Long on 3/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MasterViewController.h"
+#import "CityMapViewController.h"
+
+
+@implementation MasterViewController
+@synthesize cityMapViewController;
+#pragma mark -
+#pragma mark View lifecycle
+
+NSMutableArray *listOfMovies;
+- (void)viewDidLoad {
+	UITableViewController* temp = [[UITableViewController alloc] init];
+	temp.title =@"a";
+	self.title =@"a";
+	UITableViewController* temp2 = [[UITableViewController alloc] init];
+	
+	[self pushViewController:temp animated:YES];
+	[self pushViewController:temp2 animated:YES];
+	self.navigationController.toolbarHidden =NO;
+	UIBarButtonItem *item = [[UIBarButtonItem alloc]   
+                             initWithBarButtonSystemItem:UIBarButtonSystemItemAdd   
+                             target:self   
+                             action:@selector(doSomething)];  
+    self.navigationItem.rightBarButtonItem = item;  
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+listOfMovies = [[NSMutableArray alloc] init];
+[listOfMovies addObject:@"Training Day"];
+[listOfMovies addObject:@"Remember the Titans"];
+[listOfMovies addObject:@"John Q."];
+[listOfMovies addObject:@"The Bone Collector"];
+[listOfMovies addObject:@"Ricochet"];
+[listOfMovies addObject:@"The Siege"];
+[listOfMovies addObject:@"Malcolm X"];
+[listOfMovies addObject:@"Antwone Fisher"];
+[listOfMovies addObject:@"Courage Under Fire"];
+[listOfMovies addObject:@"He Got Game"];
+[listOfMovies addObject:@"The Pelican Brief"];
+[listOfMovies addObject:@"Glory"];
+[listOfMovies addObject:@"The Preacher's Wife"];
+
+//---set the title---
+self.navigationItem.title = @"Movies";    
+	
+[super viewDidLoad];
+//self.clearsSelectionOnViewWillAppear = NO;
+//self.contentSizeForViewInPopover = CGSizeMake(320.0, 600.0);
+}
+
+
+/*
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+*/
+/*
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+*/
+/*
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+*/
+/*
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+*/
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Override to allow orientations other than the default portrait orientation.
+    return YES;
+}
+
+
+#pragma mark -
+#pragma mark Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+	return 1;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return [listOfMovies count];
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"CellIdentifier";
+	
+    // Dequeue or create a cell of the appropriate type.
+    UITableViewCell *cell = [tableView 
+							 dequeueReusableCellWithIdentifier:CellIdentifier];
+	
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] 
+				 initWithStyle:UITableViewCellStyleDefault 
+				 reuseIdentifier:CellIdentifier] autorelease];
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+	
+    // Configure the cell.    
+    // cell.textLabel.text = 
+    //     [NSString stringWithFormat:@"Row %d", indexPath.row];
+	
+    cell.textLabel.text = [listOfMovies objectAtIndex:indexPath.row];
+	
+    return cell;
+}
+
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source.
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    }   
+}
+*/
+
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+
+#pragma mark -
+#pragma mark Table view delegate
+
+- (void)tableView:(UITableView *)aTableView 
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	
+    /*
+     When a row is selected, set the detail view controller's detail item to 
+     the item associated with the selected row.
+     */
+    //detailViewController.detailItem = 
+    //    [NSString stringWithFormat:@"Row %d", indexPath.row];
+	
+    cityMapViewController.detailItem = 
+	[NSString stringWithFormat:@"%@", 
+	 [listOfMovies objectAtIndex:indexPath.row]];    
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Relinquish ownership any cached data, images, etc. that aren't in use.
+}
+
+- (void)viewDidUnload {
+    // Relinquish ownership of anything that can be recreated in viewDidLoad or on demand.
+    // For example: self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+	 [listOfMovies release];
+    [super dealloc];
+}
+
+
+@end
+
