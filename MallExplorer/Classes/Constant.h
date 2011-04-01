b6f6979908a84b874b507c7fb9bcee2d37f97a45
Hoang Tran <hoang.trancong90@gmail.com>
2011-04-01 09:08:17 +0800

    add viviomap

---
+++ MallExplorer/Classes/Constant.h
@@ -0,0 +1,21 @@
+//
+//  Constant.h
+//  MallExplorer
+//
+//  Created by Tran Cong Hoang on 4/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// Map constant
+#define MAP_ORGIN_X = 100
+#define MAP_ORGIN_Y = 50
+#define MAP_WIDTH	= 500
+#define MAP_HEIGHT	= 500
+
+
+@protocol Constant
+
+
+@end
