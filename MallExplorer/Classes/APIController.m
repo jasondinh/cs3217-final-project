7944f8c5524b85cb73b666e225b98bd5cadef9d0
Jason Dinh <jasondinh@Jason-Dinhs-iMac-2.local>
2011-04-03 08:20:44 +0800

    APIController methods

--- MallExplorer/Classes/APIController.m
+++ MallExplorer/Classes/APIController.m
@@ -7,8 +7,46 @@
 //
 
 #import "APIController.h"
-
-
+#import "ASIHTTPRequest.h"
+#import "ASIFormDataRequest.h"
 @implementation APIController
+@synthesize delegate;
+
+- (void) getAPI: (NSString *) path {
+	NSURL *url = [NSURL URLWithString: path];
+	ASIHTTPRequest *request = [[ASIHTTPRequest alloc] initWithURL: url];
+	[request setDelegate: self];
+	[request setDidFailSelector: @selector(APIFailed:)];
+	[request setDidFinishSelector: @selector(APIFinished:)];
+	[request setDidStartSelector: @selector(APIStarted:)];
+}
+
+- (void) postAPI: (NSString *) path withData: (NSDictionary *) data {
+	
+}
+
+- (void) APIStarted: (ASIHTTPRequest *) request {
+	if ([delegate respondsToSelector: @selector(requestDidStart)]) {
+		[delegate requestDidStart];
+	}
+}
+
+- (void) APIFinished: (ASIHTTPRequest *) request {
+	//TODO: decode json and return output
+	if ([delegate respondsToSelector: @selector(requestDidLoad:)]) {
+		[delegate requestDidLoad: [request responseString]];
+	}
+}
+
+- (void) APIFailed: (ASIHTTPRequest *) request {
+	if ([delegate respondsToSelector: @selector(requestFail)]) {
+		[delegate requestFail];
+	}
+}
+
+
+- (void) dealloc {
+	[super dealloc];
+}
 
 @end
