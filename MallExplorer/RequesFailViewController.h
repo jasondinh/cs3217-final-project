c9f62de477f29006df6fc40c0c0e2b808bd69cbc
Dam Tuan Long <damlong-m@soctf-imac-026.comp.nus.edu.sg>
2011-04-12 03:15:29 +0800

    add new files

---
+++ MallExplorer/RequesFailViewController.h
@@ -0,0 +1,16 @@
+//
+//  RequesFailViewController.h
+//  MallExplorer
+//
+//  Created by Dam Tuan Long on 4/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RequesFailViewController : UIViewController {
+
+}
+
+@end
